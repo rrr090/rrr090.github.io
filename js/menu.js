cef26bfef14d1419aafb91cd0a73e1d9e70e858b
rrr090 <turtlerungo@gmail.com>
2024-10-06 03:20:00 +0500

    clear

--- js/menu.js
+++ js/menu.js
@@ -4,41 +4,38 @@ $(document).ready(function() {
     $(this).find('.submenu').slideToggle();
   });
 
-  // Evitar que el submenú se cierre al hacer clic en un enlace del submenú
   $('.submenu').click(function(e) {
-    e.stopPropagation(); // Evita que el evento se propague al elemento principal '.has-submenu'
+    e.stopPropagation(); 
   });
 });
 
 function close_info(){
-    var objeto = document.querySelector('.scene-nav-infoo'); // Reemplaza 'tuDiv' con el ID de tu div
-    objeto.style.display = 'none';
+    var object = document.querySelector('.scene-nav-infoo'); 
+    object.style.display = 'none';
     var myElement = document.getElementById("closeinfo");
     myElement.style.display = "none";
 }
 
 function Update_celular() {
-    // Obtén el tamaño de la pantalla
-    var anchoPantalla = window.innerWidth;
     
-    // Define el umbral para el ancho de pantalla de un celular
-    var umbralCelular = 600; // Puedes ajustar este valor según tus necesidades
+    var screenWidth = window.innerWidth;
     
-    // Obtén el div que deseas ocultar
     
-    // Verifica si el ancho de pantalla es menor que el umbral
-    if (anchoPantalla < umbralCelular) {
-        var infoluna = document.getElementById('infoluna'); // Reemplaza 'tuDiv' con el ID de tu div
-        var title = document.getElementById('title'); // Reemplaza 'tuDiv' con el ID de tu div
-        // Oculta el div si se cumple la condición
+    var shadow = 600; 
+    
+    
+    if (screenWidth < shadow) {
+        var infoluna = document.getElementById('infoluna');
+        var title = document.getElementById('title'); 
+      
         title.innerText = "";
         infoluna.style.display = 'none';
         var sceneNavMenu = document.querySelector(".scene-nav-menu");
-        // Cambia el ancho del div
-        sceneNavMenu.style.width = "160px"; // Puedes ajustar este valor según sea necesario
+       
+        sceneNavMenu.style.width = "160px"; 
         var scenenavinfoo = document.querySelector(".scene-nav-infoo");
-        // Cambia el ancho del div
-        scenenavinfoo.style.width = "55%"; // Puedes ajustar este valor según sea necesario
+       
+        scenenavinfoo.style.width = "55%"; 
     }
 }
 
@@ -47,24 +44,24 @@ let menu = true;
 
 function togglemenu() {
     if (menu) {
-         var menuu = document.getElementById('menu'); // Reemplaza 'tuDiv' con el ID de tu div
+         var menuu = document.getElementById('menu');
          menuu.style.display = 'none';
          var menuClose = document.getElementById("MenuClose");
          menuClose.innerText = "+";
     } else {
-       var menuu = document.getElementById('menu'); // Reemplaza 'tuDiv' con el ID de tu div
+       var menuu = document.getElementById('menu');
          menuu.style.display = 'block';
          var menuClose = document.getElementById("MenuClose");
          menuClose.innerText = "-";
     }
     menu = !menu;
         
-    // Obtén el tamaño de la pantalla
-    var anchoPantalla = window.innerWidth;
+ 
+    var screenWidth = window.innerWidth;
     
-    // Define el umbral para el ancho de pantalla de un celular
-    var umbralCelular = 600; // Puedes ajustar este valor según tus necesidades
-    if(anchoPantalla<umbralCelular){
+   
+    var shadow = 600;
+    if(screenWidth<shadow){
         
     }
 }
